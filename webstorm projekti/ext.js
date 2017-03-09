3711815c5b9fe750b50167579bde55e238e3de41
TiborPfarrer <tibor.pfarrer@gmail.com>
2017-03-09 10:13:34 +0200

    Add files via upload

---
+++ webstorm projekti/ext.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Ucenik on 23.2.2017..
+ */
+function myFunction()
+{
+    document.getElementById("demo").innerHTML="Paragraph changed.";
+    alert("ovdje ide poruka koja ce se prikazati u prozoru");
+    confirm("molio odaberite");
+    prompt("molimo unesite vase ime","Luke Skywalker");
+}
+document.write("<h1>Dobar Dan!</h1><p>JooooooJO incoming!</p>");
---
+++ webstorm projekti/gumb09.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Ucenik on 9.3.2017..
+ */
+/*function ispis()
+{
+    document.getElementById("brojevi")
+    for (var j=0; j!=16; j++)
+    {
+        document.write("broj je ");
+        document.write(j);
+        document.write("<br>");
+    }
+}*/
+/*function parni() {
+    document.getElementById("brojevi")
+    for (var j = 0; j != 16; j++) {
+        if (j==0)
+        {
+            document.write(j);
+            document.write(" nije ni paran ni neparan")
+            document.write("<br>")
+        }
+        else if (j % 2 == 0) {
+            document.write(j);
+            document.write(" je paran ");
+            document.write("<br>");
+        }
+        else if (j % 2 != 0) {
+            document.write(j);
+            document.write(" je neparan ");
+            document.write("<br>");
+        }
+    }
+}*/
+function unos()
+{
+    document.getElementById("brojevi");
+    var broj=prompt();
+    var poc=prompt();
+    for (var i=poc;i<=broj;i++)
+    {
+        text+=
+    }
+}
